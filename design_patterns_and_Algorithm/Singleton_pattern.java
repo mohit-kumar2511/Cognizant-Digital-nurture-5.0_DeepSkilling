a002ef5dd716c9a3b6e89fcf32c0fd44a108591f
Mohit Kumar <mohitkumarsolanki25112005@gmail.com>
2026-06-22 11:45:01 +0530

    [Mohit Kumar]+Singleton_pattern

---
+++ design_patterns_and_Algorithm/Singleton_pattern.java
@@ -0,0 +1,37 @@
+class Singleton_pattern {
+
+    static class Logger {
+
+        private static Logger instance;
+
+        private Logger() {
+            System.out.println("Logger object created");
+        }
+
+        public static Logger getInstance() {
+            if (instance == null) {
+                instance = new Logger();
+            }
+            return instance;
+        }
+
+        public void log(String message) {
+            System.out.println("Log: " + message);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+
+        logger1.log("First Message");
+        logger2.log("Second Message");
+
+        if (logger1 == logger2) {
+            System.out.println("Only one Logger instance exists.");
+        } else {
+            System.out.println("Multiple instances created.");
+        }
+    }
+}
